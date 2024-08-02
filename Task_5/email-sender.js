5a76fc333ecebc3bdd6ea0d52aa80a02179a12bc
Dennis <dennojunior@gmail.com>
2024-08-02 21:59:27 +0300

    retested the email but its still causing an error

--- Task_5/email-sender.js
+++ Task_5/email-sender.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'elegidodigital@gmail.com', //your email address
-    pass: '***********', // type your password
+    pass: '********', // type your password
   },
 });
 //email structure
